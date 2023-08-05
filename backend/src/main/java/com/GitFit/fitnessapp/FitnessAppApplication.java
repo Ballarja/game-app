6926753a9e460e118531ca181f6c68a4064de9ba
Ballarja <JamesBallard87@hotmail.com>
2023-08-04 22:18:37 -0500

    springboot added to backend

---
+++ backend/src/main/java/com/GitFit/fitnessapp/FitnessAppApplication.java
@@ -0,0 +1,13 @@
+package com.GitFit.fitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/GitFit/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GitFit.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
