bc85a79edc6f749701b1349b0fd2573d89d2b4cf
James Ballard <JamesBallard87@hotmail.com>
2024-04-20 14:35:31 -0500

    added generated fields for MySQL database in GameModel class

--- backend/src/main/java/com/GameApp/model/GameModel.java
+++ backend/src/main/java/com/GameApp/model/GameModel.java
@@ -6,12 +6,85 @@ import java.util.UUID;
 
 @Entity
 public class GameModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String gameDescription;
+    private String genre;
     private int timePlayed;
+    private boolean owned;
+    private int price;
+    private boolean currentSale;
+
+    public boolean isCurrentSale() {
+        return currentSale;
+    }
+
+    public void setCurrentSale(boolean currentSale) {
+        this.currentSale = currentSale;
+    }
+
+    public boolean isOwned() {
+        return owned;
+    }
+
+    public void setOwned(boolean owned) {
+        this.owned = owned;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGameDescription() {
+        return gameDescription;
+    }
+
+    public void setGameDescription(String gameDescription) {
+        this.gameDescription = gameDescription;
+    }
+
+    public int getTimePlayed() {
+        return timePlayed;
+    }
+
+    public void setTimePlayed(int timePlayed) {
+        this.timePlayed = timePlayed;
+    }
+
+
 
 }
