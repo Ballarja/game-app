8b8bc08931ceaee4fd58ac2729782643d4372606
James Ballard <JamesBallard87@hotmail.com>
2024-05-11 13:49:20 -0500

    Added values @annotations to LoginModel.Java to be able to store values in MySQL database

--- backend/src/main/java/com/GameApp/model/LoginModel.java
+++ backend/src/main/java/com/GameApp/model/LoginModel.java
@@ -1,4 +1,41 @@
 package com.GameApp.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
 public class LoginModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id = UUID.randomUUID();
+    private String email;
+    private String password;
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
+
+
