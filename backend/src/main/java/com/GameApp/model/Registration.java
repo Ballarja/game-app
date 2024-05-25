6d56ffdf2775c841222c48ad3d07887c69590b86
James Ballard <JamesBallard87@hotmail.com>
2024-05-24 23:27:02 -0500

    updated fields in Registration.java

--- backend/src/main/java/com/GameApp/model/Registration.java
+++ backend/src/main/java/com/GameApp/model/Registration.java
@@ -8,27 +8,16 @@ import java.util.UUID;
 public class Registration {
     @Id
     private UUID id = UUID.randomUUID();
-    private String name;
-    private String company;
+    private String userName;
     private String email;
     private String password;
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
     public UUID getId() {
         return id;
     }
